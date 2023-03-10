98c3f4b573a266d2d6ae72ede96c44204ae4f701
felipeghapa <felipef32@hotmail.com>
2023-03-10 13:42:55 -0300

    test

--- src/models/Foto.js
+++ src/models/Foto.js
@@ -1,34 +1,41 @@
 import Sequelize, { Model } from 'sequelize';
 // import appConfig from '../config/appConfig';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const url = process.env.APP_URL;
 
 export default class Foto extends Model {
   static init(sequelize) {
-    super.init({
-      originalname: {
-        type: Sequelize.STRING,
-        defaultValue: '',
-        validate: {
-          notEmpty: {
-            msg: 'Campo não pode ficar vazio',
+    super.init(
+      {
+        originalname: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            notEmpty: {
+              msg: 'Campo não pode ficar vazio',
+            },
+          },
+        },
+        filename: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            notEmpty: {
+              msg: 'Campo não pode ficar vazio',
+            },
+          },
+        },
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `${url}/images/${this.getDataValue('filename')}`;
           },
         },
       },
-      filename: {
-        type: Sequelize.STRING,
-        defaultValue: '',
-        validate: {
-          notEmpty: {
-            msg: 'Campo não pode ficar vazio',
-          },
-        },
-      },
-      url: {
-        type: Sequelize.VIRTUAL,
-        get() {
-          return `http://34.125.20.150:81/images/${this.getDataValue('filename')}`;
-        },
-      },
-    }, { sequelize, tableName: 'fotos' });
+      { sequelize, tableName: 'fotos' },
+    );
     return this;
   }
 
